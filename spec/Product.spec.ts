c98ebb07f4e0bc609130bb6a90e51e57560bd114
rafael-camacho <racamacho@falabella.cl>
2020-08-30 19:42:37 -0400

    adding product update price initial validations and main functions and adding specs

--- spec/Product.spec.ts
+++ spec/Product.spec.ts
@@ -1,11 +1,74 @@
-import { Product } from '../src/Product';
+import { Product } from '../src/product';
 import { expect } from 'chai';
 
 describe('product', () => {
-  it('should initialize a Product of name Full Coverage', () => {
-    const product = new Product('Full Coverage', 20, 80);
-    expect(product.name).to.equal('Full Coverage');
+  it('should initialize a Product of name Product Test', () => {
+    const product = new Product('Product Test', 20, 80);
+    expect(product.name).to.equal('Product Test');
     expect(product.sellIn).to.equal(20);
     expect(product.price).to.equal(80);
+  });
+
+  describe('updatePrice', () => {
+    it('should decrease price by 2 if the sell in is negative', () => {
+      const product = new Product('Product Test', -1, 4);
+      product.updatePrice();
+      expect(product.price).to.equal(2);
+    })
+
+    it('should decrease price by 1 if the sell in is not negative', () => {
+      const product = new Product('Product Test', 1, 4);
+      product.updatePrice();
+      expect(product.price).to.equal(3);
+    })
+
+    it('should reset the price to 0 if the price is negative', () => {
+      const product = new Product('Product Test', -1, 1);
+      product.updatePrice();
+      expect(product.price).to.equal(0);
+    })
+
+    it('should set the max price if the price is higher than the max price', () => {
+      const product = new Product('Product Test', 1, 52);
+      product.updatePrice();
+      expect(product.price).to.equal(50);
+    })
+  })
+
+  describe('resetNegativePrice', () => {
+    it('should reset price to 0', () => {
+      const product = new Product('Product Test', 0, -1);
+      product.resetNegativePrice();
+      expect(product.price).to.equal(0);
+    })
+  })
+
+  describe('updatePriceBy', () => {
+    it('should increase price to 2', () => {
+      const product = new Product('Product Test', 0, 1);
+      product.operation = '+=';
+      product.updatePriceBy(1);
+      expect(product.price).to.equal(2);
+    })
+
+    it('should decrease price to 1', () => {
+      const product = new Product('Product Test', 0, 2);
+      product.updatePriceBy(1);
+      expect(product.price).to.equal(1);
+    })
+
+    it('should decrease price to 0', () => {
+      const product = new Product('Product Test', 0, 2);
+      product.updatePriceBy(2);
+      expect(product.price).to.equal(0);
+    })
+  })
+
+  describe('reachMaxPrice', () => {
+    it('should set the price to 50 ', () => {
+      const product = new Product('Product Test', 0, 1);
+      product.reachMaxPrice();
+      expect(product.price).to.equal(50);
+    })
   })
 })
--- src/Product.ts
+++ src/Product.ts
@@ -1,11 +1,48 @@
 export class Product {
   public name: String;
-  public sellIn: Number;
-  public price: Number;
+  public sellIn: any;
+  public price: any;
+  public num: number;
+  public operation: String = '-=';
+
+  SELL_IN_DECREASE = 1;
+  PRICE_DECREASE = 1;
+  PRICE_INCREASE = 1;
+  PRICE_MAX = 50
 
   constructor(name, sellIn, price) {
     this.name = name;
     this.sellIn = sellIn;
     this.price = price;
+  }
+
+  updatePrice() {
+    this.sellIn -= this.SELL_IN_DECREASE;
+    this.sellIn < 0 && this.operation === '-=' ? this.updatePriceBy(2) : this.updatePriceBy(1);
+
+    this.checkPriceLimits();
+  }
+
+  checkPriceLimits() {
+    switch (true) {
+    case (this.price < 0):
+      this.resetNegativePrice();
+      break;
+    case (this.price > this.PRICE_MAX):
+      this.reachMaxPrice();
+      break;
+    } 
+  }
+
+  updatePriceBy(number) {
+    eval(`this.price${this.operation}${number}`.toString())
+  }
+
+  resetNegativePrice() {
+    this.price = 0;
+  }
+
+  reachMaxPrice() {
+    this.price = this.PRICE_MAX;
   }
 }
